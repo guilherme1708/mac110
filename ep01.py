de79cfa161d32e203175bf74064e8d7c9a25c73a
guilherme1708 <guilherme2.navarro@usp.br>
2018-09-16 13:27:32 -0300

    Commit dos EP's

---
+++ ep01.py
@@ -0,0 +1,13 @@
+"""
+  Nome : Guilherme Navarro
+  NUSP : 8943160
+  Turma: 41
+  Prof.: Hitoshi
+
+  """
+def main():
+    print("Verificar se o aluno foi aprovado no curso\n")
+    i=int(input("Digite uma nota: "))
+    a=i>=50
+    print(a)
+main()
